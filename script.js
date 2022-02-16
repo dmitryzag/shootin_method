87778fe9615d042349d0b829a1a97f0264bf3232
Dmitry <zagibalovdmitry@gmail.com>
2022-02-16 19:12:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+new Chartist.Line('.ct-chart', {
+    labels: [],
+    series: []
+  }, {
+    fullWidth: true,
+    chartPadding: {
+      right: 40
+    }
+  });
+
+const fRunge = (array,n,e,c,a,b) => {
+  const h = (b -a) / n;
+
+  for(let i = 0; i < )
+
+};
